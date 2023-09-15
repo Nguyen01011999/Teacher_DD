09ae42b7d01c65d3335e430055b249c667653113
Nguyen01011999 <81405942+Nguyen01011999@users.noreply.github.com>
2023-09-15 13:12:08 +0700

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://learn-mern-scbg.onrender.com'
+    'https://learn-mern-scbg.onrender.com',
+    'https://fe-mern-wx6v.onrender.com'
 ]
 
 module.exports = allowedOrigins
